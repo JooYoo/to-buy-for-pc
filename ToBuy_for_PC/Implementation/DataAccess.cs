8019f7b5a431877185c1c8dd8ca8bcf41e8194da
JooYoo <jooyoo@outlook.com>
2018-05-13 08:04:26 +0200

    same

---
+++ ToBuy_for_PC/Implementation/DataAccess.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using Newtonsoft.Json;
+using ToBuy_for_PC.DataContract;
+using ToBuy_for_PC.OperationContract;
+
+namespace ToBuy_for_PC.Implementation
+{
+    public class DataAccess:IDataAccess
+    {
+        public void ToSave(ObservableCollection<ToBuy> toBuys)
+        {
+            using (StreamWriter file = File.CreateText(@"ToBuyData.json"))
+            {
+                var serializer = new JsonSerializer();
+                serializer.Formatting = Formatting.Indented;    // Json formating 
+                serializer.Serialize(file, toBuys);
+            }
+        }
+
+        public List<ToBuy> ToLoad()
+        {
+            try
+            {
+                using (StreamReader file = File.OpenText(@"ToBuyData.json"))
+                {
+                    var deserializer = new JsonSerializer();
+                    List<ToBuy> result = (List<ToBuy>)deserializer.Deserialize(file, typeof(List<ToBuy>));
+                    
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                return new List<ToBuy>()
+                {
+                    new ToBuy{Name = "sample. apple", IsDone = false},
+                    new ToBuy{Name = "sample. pizza", IsDone = false},
+                    new ToBuy{Name = "sample. beer", IsDone = false}
+                };
+            }
+        }
+    }
+}
---
+++ ToBuy_for_PC/OperationContract/IDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using ToBuy_for_PC.DataContract;
+
+namespace ToBuy_for_PC.OperationContract
+{
+    public interface IDataAccess
+    {
+        void ToSave(ObservableCollection<ToBuy> toBuys);
+        List<ToBuy> ToLoad();
+    }
+}
