b07db70c4b25c25f1eb2ec070b1a4ac0456ad012
JooYoo <jooyoo@outlook.com>
2018-05-16 11:39:04 +0200

    finished ToSaveTests

---
+++ ToBuy_for_PC_Unit Test/ToLoadTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToBuy_for_PC.DataContract;
+
+namespace ToBuy_for_PC_Unit_Test
+{
+    [TestClass]
+    public class ToLoadTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+
+
+        //public ObservableCollection<ToBuy> RawToBuys()
+        //{
+        //    return  new ObservableCollection<ToBuy>
+        //    {
+        //        new ToBuy{Name = "ToBuy01", IsDone = false},
+        //        new ToBuy{Name = "ToBuy02", IsDone = false},
+        //        new ToBuy{Name = "ToBuy03", IsDone = true}
+        //    };
+        //}
+
+    }
+}
--- ToBuy_for_PC_Unit Test/ToSaveTests.cs
+++ ToBuy_for_PC_Unit Test/ToSaveTests.cs
@@ -63,21 +63,62 @@ namespace ToBuy_for_PC_Unit_Test
         }
 
         [TestMethod]
-        public void TestMethod2()
+        public void ToSaveClearCommandTest()
         {
-            //Preparation
+            // fake LoadData
             var dataAccessMock = new Mock<IDataAccess>();
-            dataAccessMock.Setup(m => m.ToLoad()).Returns(new List<ToBuy> { new ToBuy { IsDone = false, Name = "Test" } });
-            var viewModel = new MainWindowViewModel(dataAccessMock.Object);
-            viewModel.WantBuy = "Test123";
-            var addCommand = new AddCommand(viewModel);
+            dataAccessMock.Setup(m => m.ToLoad()).Returns(new List<ToBuy>());
 
-            //Test
+            // input one Item
+            var viewModel = new MainWindowViewModel(dataAccessMock.Object); // instance MainWindowViewModel
+            viewModel.WantBuy = "newItem";
+            
+            // pressed add button
+            var addCommand = new AddCommand(viewModel);
             addCommand.Execute(null);
 
-            //Validation
-            dataAccessMock.Verify(m => m.ToSave(It.IsAny<ObservableCollection<ToBuy>>()), Times.Exactly(1));
-            Assert.AreEqual(viewModel.WantBuy, string.Empty);
+            // before clear
+            Assert.AreEqual(viewModel.ToBuys.Count, 1);
+            Assert.AreEqual(viewModel.ToBuys[0].Name, "newItem");
+            Assert.IsFalse(viewModel.ToBuys[0].IsDone);
+
+            // clean item up
+            var clearCommand = new ClearCommand(viewModel);
+            clearCommand.Execute(null);
+
+            // after clear
+            Assert.AreEqual(viewModel.ToBuys.Count, 0);
+        }
+
+        [TestMethod]
+        public void ToSaveRemoveCommandTest()
+        {
+            // fake LoadData
+            var dataAccessMock = new Mock<IDataAccess>();
+            dataAccessMock.Setup(m => m.ToLoad()).Returns(new List<ToBuy>());
+
+            // input one Item
+            var viewModel = new MainWindowViewModel(dataAccessMock.Object); // instance MainWindowViewModel
+            viewModel.WantBuy = "newItem";
+
+            // pressed add button
+            var addCommand = new AddCommand(viewModel);
+            addCommand.Execute(null);
+
+            // before remove
+            Assert.AreEqual(viewModel.ToBuys.Count, 1);
+            Assert.AreEqual(viewModel.ToBuys[0].Name, "newItem");
+            Assert.IsFalse(viewModel.ToBuys[0].IsDone);
+
+            // set selected Item
+            viewModel.SelectedToBuy = viewModel.ToBuys[0];
+
+            // delete selected
+            var removeSelectedCommand = new RemoveSelectedCommand(viewModel);
+            removeSelectedCommand.Execute(null);
+
+            // after clear
+            Assert.AreEqual(viewModel.ToBuys.Count, 0);
         }
     }
 }
