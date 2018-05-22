b6808614df50561cadbe52ba0c3bf7abf81d24e9
JooYoo <jooyoo@outlook.com>
2018-05-22 15:39:07 +0200

    added top background image

--- ToBuy_for_PC/MainWindow.xaml.cs
+++ ToBuy_for_PC/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Animation;
-using ToBuy_for_PC.ListWindow;
 
 namespace ToBuy_for_PC
 {
